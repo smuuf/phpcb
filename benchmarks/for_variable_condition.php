ccfb13a7a060a122ba0d802e5e20b83ce02589ad
Smuuf <premavansmuuf+git@gmail.com>
2021-12-21 16:17:22 +0100

    Updated example benchmark.

--- benchmarks/for_variable_condition.php
+++ benchmarks/for_variable_condition.php
@@ -6,27 +6,19 @@ $bench = new \Smuuf\Phpcb\PhpBenchmark;
 const COUNT = 100;
 
 $bench->addBench(function() {
-
 	for ($i = 1; $i <= COUNT; $i++) {}
-
 });
 
 $bench->addBench(function() {
-
 	for ($i = COUNT; $i > 0; $i--) {}
-
 });
 
 $bench->addBench(function() {
-
 	for ($i = COUNT; $i--;) {}
-
 });
 
 $bench->addBench(function() {
-
 	for ($i = -COUNT; $i++;) {}
-
 });
 
 $bench->run();
